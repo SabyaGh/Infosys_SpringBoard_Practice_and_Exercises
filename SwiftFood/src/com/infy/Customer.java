56521206e31150f778e6db3ea8bb7bfd8707d15f
Sabyasachi Ghosh <163895001+SabyaGh@users.noreply.github.com>
2026-04-01 16:52:48 +0530

    payBills Method added to the Customer class,and foodDetails are Checking

--- SwiftFood/src/com/infy/Customer.java
+++ SwiftFood/src/com/infy/Customer.java
@@ -14,5 +14,13 @@ public class Customer {
 		System.out.println("Address : " + address);
 		System.out.println();
 	}
+	
+	public void payBill(double totalPrice, double discountPercentage) {
+		System.out.println("Calculating final amount to be paid......");
+		double priceAfterDiscount = totalPrice * (1 - (discountPercentage / 100));
+		System.out.println("Hi " + customerName
+				+ ", your final bill amount after discount is: "
+				+ (int) (priceAfterDiscount * 100) / 100.0);
+	}
 
 }

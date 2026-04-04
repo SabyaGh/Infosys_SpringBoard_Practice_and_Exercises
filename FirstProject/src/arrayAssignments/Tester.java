841636a1eeb778c99095bc11e3817ea5c25e1883
Sabyasachi Ghosh <163895001+SabyaGh@users.noreply.github.com>
2026-04-04 23:27:08 +0530

    Added Array Exercise 1

---
+++ FirstProject/src/arrayAssignments/Tester.java
@@ -0,0 +1,18 @@
+package arrayAssignments;
+
+public class Tester {
+	 public static int calculateSumOfEvenNumbers(int[] numbers){
+	        int sum=0;
+	        for(int num : numbers){
+	            if(num%2==0){
+	                sum+=num;
+	            }
+	        }
+	        return sum;
+	    }
+	    
+		public static void main(String[] args) {
+			int[] numbers = {68,79,86,99,23,2,41,100};
+			System.out.println("Sum of even numbers: " +calculateSumOfEvenNumbers(numbers));
+		}
+	}

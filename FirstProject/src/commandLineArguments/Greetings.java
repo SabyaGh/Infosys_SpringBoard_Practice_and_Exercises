d8251a8f3ed4dde4c8e3c5faf9e5212899e33ed3
Sabyasachi Ghosh <163895001+SabyaGh@users.noreply.github.com>
2026-04-04 23:32:55 +0530

    Add comments explaining CLI argument usage
    Add inline comments to Greetings.java describing how to pass arguments from the command line or via IDE run configuration. This is a documentation-only change to clarify the purpose for newcomers; no functional code changes were made.

--- FirstProject/src/commandLineArguments/Greetings.java
+++ FirstProject/src/commandLineArguments/Greetings.java
@@ -1,5 +1,6 @@
 package commandLineArguments;
-
+// in this class we are trying to pass argumemts from 
+//the command line in CLI or from the arguments in IDE
 public class Greetings {
 	public static void main(String[] args) {
 		System.out.println("Hello "+args[0]);

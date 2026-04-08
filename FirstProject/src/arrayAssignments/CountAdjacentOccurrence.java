402b5443e8f53e60181410f23afaa3902ff52982
Sabyasachi Ghosh <163895001+SabyaGh@users.noreply.github.com>
2026-04-08 16:38:38 +0530

    Array Assignment_6
    Find and return the count of the adjacent occurrence of a number in the numbers array passed to the findTotalCount() method.

---
+++ FirstProject/src/arrayAssignments/CountAdjacentOccurrence.java
@@ -0,0 +1,19 @@
+package arrayAssignments;
+
+public class CountAdjacentOccurrence {
+	public static int findTotalCount(int[] numbers) {
+		int count = 0;
+		for(int i = 0;i<numbers.length-1;i++) {
+			if(numbers[i]==numbers[i+1]) {
+				count++;
+			}
+		}
+        return count;
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 1, 5, 100, -20, 6, 0, 0 };
+		System.out.println("Count of adjacent occurrence: "+findTotalCount(numbers));
+	}
+
+}
